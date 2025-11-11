7f430bda9da28b0e4a20e50c76b71f54e8b8c9a3
indragans <wahyuindea345@gmail.com>
2025-11-11 19:49:50 +0700

    update karakter

---
+++ Assets/Script/Actions/ActionDialog.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionDialog : SceneAction
+{
+    public override void interaction()
+    {
+        throw new System.NotImplementedException();
+    }
+    
+}
