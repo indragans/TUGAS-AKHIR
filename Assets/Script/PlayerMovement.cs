84d56a039eb1dfe91a575ad6a839c36ecb562705
indragans <wahyuindea345@gmail.com>
2025-11-20 19:33:10 +0700

    update animasi karakter

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,38 +1,57 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovementSimpleAnim : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 10f;
+    private float originalScaleX;
 
     private Rigidbody2D rb;
-    private bool isGrounded = true; // sederhana, nanti bisa diganti pake ground check
+    private Animator anim;
+    private bool isGrounded = true;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        originalScaleX = transform.localScale.x;
     }
 
     void Update()
     {
-        // Gerak kiri/kanan
         float move = Input.GetAxisRaw("Horizontal");
+
+        // Gerak kiri kanan
         rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
+
+        // Animasi jalan / idle
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        // Flip kiri-kanan sederhana (TIDAK mengubah ukuran)
+        if (move != 0)
+        {
+            transform.localScale = new Vector3(
+                originalScaleX * Mathf.Sign(move),
+                transform.localScale.y,
+                transform.localScale.z
+            );
+        }
 
         // Lompat
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            anim.SetBool("IsJumping", true);
             isGrounded = false;
         }
     }
 
-    // Supaya bisa lompat lagi pas nyentuh tanah (pakai collider)
-    void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.contacts[0].normal.y > 0.5f)
         {
             isGrounded = true;
+            anim.SetBool("IsJumping", false);
         }
     }
 }
